0edb04146f7ba873ed37ee464cb8a61e136c076b
2Kyoungh <127364874+2Kyoungh@users.noreply.github.com>
2023-03-30 15:31:25 +0900

    "ALLOWED_HOSTS 변경"

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-sf6l18ys!hrmhg_2wh5&%x27b*)pxs3#c8xv^#pi+pvcu=ih1p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['15.165.151.177']
 
 
 # Application definition
